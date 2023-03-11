11d8fb45ceb4979f678b2786ce698079f92af926
Jon Roeske <jproeske22@gmail.com>
2023-03-10 16:16:09 -0800

    updated dashboard

--- frontend/kernel-config-manager/src/Dashboard.js
+++ frontend/kernel-config-manager/src/Dashboard.js
@@ -6,7 +6,18 @@ export default function Dashboard() {
     <h1>Kernel Configuration Manager</h1>    
     </div>
     
-    
+    <div style={{ padding: 20, display: 'flex', justifyContent: 'center'}}>
+    <table>
+      <thead>
+        <th>Tunable Class</th>
+        <th></th>
+        <th>Parameter</th>
+        <th></th>
+        <th>Value</th>
+        <th></th>
+      </thead>
+    </table>
+    </div>
 
     <div style={{ padding: 20, display: 'flex', justifyContent: 'center'}}>
     <button>Save & Update</button>
