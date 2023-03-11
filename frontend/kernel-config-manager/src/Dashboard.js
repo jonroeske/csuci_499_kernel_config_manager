72867f831ea5d21305216536743d4686015414a9
Jon Roeske <jproeske22@gmail.com>
2023-03-11 13:30:09 -0800

    frontend work

--- frontend/kernel-config-manager/src/Dashboard.js
+++ frontend/kernel-config-manager/src/Dashboard.js
@@ -1,26 +1,46 @@
 import React from 'react';
+import Bootstrap from 'bootstrap';
 
 export default function Dashboard() {
   return(
-    <><div style={{ padding: 20, display: 'flex', justifyContent: 'left' }}>
-    <h1>Kernel Configuration Manager</h1>    
+    <><nav class="navbar bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand">Kernel Configuration Manager</a>
+      <form class="d-flex" role="search">
+        <button class="btn btn-outline-dark" type="submit">Logout</button>
+      </form>
     </div>
+  </nav>
     
-    <div style={{ padding: 20, display: 'flex', justifyContent: 'center'}}>
-    <table>
-      <thead>
-        <th>Tunable Class</th>
-        <th></th>
-        <th>Parameter</th>
-        <th></th>
-        <th>Value</th>
-        <th></th>
-      </thead>
-    </table>
+    <div class="d-flex justify-content-center">  
+      <div class="spinner-border m-5" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </div>
+
+    <div class="container text-center">
+      <div class="row row-cols-3">
+        <div class="col">Tunable Class</div>
+        <div class="col">Parameter</div>
+        <div class="col">Value</div>
+      </div>
     </div>
 
     <div style={{ padding: 20, display: 'flex', justifyContent: 'center'}}>
-    <button>Save & Update</button>
+      <div class="mb3">
+      <input type="text" class="form-control" id="valueInput" placeholder="Value"/>
+      </div>
+      
+      <div class="dropdown">  
+        <a class="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          Update
+        </a>
+
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" href="#">Runtime</a></li>
+          <li><a class="dropdown-item" href="#">Runtime & Persistent</a></li>
+        </ul>
+      </div>
     </div></>
   );
 }
--- frontend/kernel-config-manager/src/Login.js
+++ frontend/kernel-config-manager/src/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Bootstrap from 'bootstrap';
 
 export default function Login() {
   return (
@@ -8,13 +9,19 @@ export default function Login() {
     <div style={{ padding: 1, display: 'flex', justifyContent: 'center' }}>
         <b>Login using system credentials</b>
     </div>
-    <div style={{ padding: 20, display: 'flex', justifyContent: 'center'}}>
-      <input type="text" placeholder="username"/>
-      <div style={{ padding:10 }}/>
-      <input type="password" placeholder="password"/>
-    </div>
-    <div style={{ padding: 20, display: 'flex', justifyContent: 'center'}}>
-      <button>Login</button>
+    <div class="form-signin w-50 m-auto">
+      <div style={{ padding: 20, display: 'flex', justifyContent: 'center'}}>
+        <input id="floatingInput" class="form-control" type="text" placeholder="Username"/>
+        <div style={{ padding: 10, display: 'flex', justifyContent: 'center'}}/>
+        <input id="floatingPassword" class="form-control" type="password" placeholder="Password"/>
+      </div>
+      <div style={{ padding: 20, display: 'flex', justifyContent: 'center'}}>
+        <button class="btn btn-outline-dark" type="submit">Login</button>
+        <button class="btn btn-outline-dark" type="button" disabled>
+          <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+           Logging In...
+        </button>
+      </div>
     </div>
     </>
   );
