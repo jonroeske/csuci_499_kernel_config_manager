53bcab0305ca5ede7baf1887fff7fd653839a074
Jon Roeske <jproeske22@gmail.com>
2023-03-12 16:33:52 -0700

    endpoints created

--- frontend/kernel-config-manager/server/index.js
+++ frontend/kernel-config-manager/server/index.js
@@ -1,20 +1,72 @@
 const express = require('express');
-const path = require('path'); // NEW
-
+const path = require('path');
+var cp = require('child_process');
 const app = express();
 const port = process.env.PORT || 3001;
-const DIST_DIR = path.join(__dirname, '../dist'); // NEW
-const HTML_FILE = path.join(DIST_DIR, 'index.html'); // NEW
-const mockResponse = {
-  foo: 'bar',
-  bar: 'foo'
-};
-app.use(express.static(DIST_DIR)); // NEW
-app.get('/api', (req, res) => {
-  res.send(mockResponse);
+const DIST_DIR = path.join(__dirname, '../dist');
+const HTML_FILE = path.join(DIST_DIR, 'index.html');
+app.use(express.static(DIST_DIR));
+app.post('/login', function requestHandler(req, res) {
+    var child = cp.spawn('../../scripts/login.sh', [req.body.user, req.body.password])
+    child.stdout.on('data', function(data) {
+        if (data.trim().includes('1')){
+            res.send(req.body.user);
+        }
+        else{
+            res.send('');
+        }
+    });
+    child.stderr.on('err', function(err){
+        res.send(err);
+    });
+});
+app.post('/get/value', function requestHandler(req, res) {
+    var child = cp.spawn('../../scripts/get_value.sh', [req.body.class, req.body.parameter])
+    child.stdout.on('data', function(data) {
+        res.send(data);
+    });
+    child.stderr.on('err', function(err){
+        res.send(err);
+    });
+});
+app.post('/get/params', function requestHandler(req, res) {
+    var child = cp.spawn('../../scripts/get_params.sh', [req.body.class])
+    child.stdout.on('data', function(data) {
+        res.send(data);
+    });
+    child.stderr.on('err', function(err){
+        res.send(err);
+    });
+});
+app.get('/get/classes', (req, res) => {
+    var child = cp.spawn('../../scripts/get_tunable_classes.sh')
+    child.stdout.on('data', function(data){
+        res.send(data);
+    });
+    child.stderr.on('err', function(err){
+        res.send(err);
+    });
+});
+app.post('/set/runtime', function requestHandler(req, res) {
+    var child = cp.spawn('../../scripts/set_runtime_param.sh', [req.body.class, req.body.parameter, req.body.value])
+    child.stdout.on('data', function(data) {
+        res.send(data);
+    });
+    child.stderr.on('err', function(err){
+        res.send(err);
+    });
+});
+app.post('/set/persistent', function requestHandler(req, res) {
+    var child = cp.spawn('../../scripts/set_persistent_param.sh', [req.body.class, req.body.parameter, req.body.value])
+    child.stdout.on('data', function(data) {
+        res.send(data);
+    });
+    child.stderr.on('err', function(err){
+        res.send(err);
+    });
 });
 app.get('/', (req, res) => {
- res.sendFile(HTML_FILE); // EDIT
+ res.sendFile(HTML_FILE);
 });
 app.listen(port, function () {
  console.log('App listening on port: ' + port);
--- scripts/login.sh
+++ scripts/login.sh
@@ -5,8 +5,10 @@ username_check=$(/usr/bin/getent passwd "$username")
 if [ "$username_check" == *"$username"* ]; then
     password_check=$(/usr/bin/su "$username" -c 'echo' <<< "$password")
     if [ "$password_check" == "" ]; then
-        exit 1;
+        echo '1';
+	exit;
     fi
 else
-    exit 0;
+    echo '0';
+    exit;
 fi
